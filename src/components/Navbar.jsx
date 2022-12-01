40619c39c30221008198ec0495b1608a5e6b11b5
Thuy Linh Nguyen-Kinnel <ntlinh2604@gmail.con>
2022-12-01 19:09:52 +0100

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import logo from "../assets/airbnb-logo.png"
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={logo} className="nav--logo"/>
+        </nav>
+    )
+}
